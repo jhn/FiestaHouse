751fa691f3afa73f2366146883327374c710c7f5
Johan Mena <johan.menac@gmail.com>
2014-04-06 11:57:29 -0400

    YOLOOOO

--- public/js/main.js
+++ public/js/main.js
@@ -45,19 +45,30 @@ $(document).ready(function() {
       // });
     });
 
-  $('#foodProceed').click(function (e) {
-    // don't submit yet
-    // e.preventDefault();
-    $.post("/deliverydotcombooze", $( "#deliveryForm" ).serialize(), function(data) {
-      var payload = $.parseJSON(data);
-      var allMerchants = payload.merchants;
-      var random = Math.floor(getRandomArbitrary(0, allMerchants.length));
-      var singleMerchant = allMerchants[random];
-      debugger;
-      var randomMenu;
-      var itemPayload;
-    });
-  });
+  // $('#foodProceed').click(function (e) {
+  //   // don't submit yet
+  //   // e.preventDefault();
+  //   $.post("/deliverydotcombooze", $( "#deliveryForm" ).serialize(), function(data) {
+  //     var payload = $.parseJSON(data);
+
+  //       var mydiv = document.createElement( "div" );
+  //       var elemToAttach = "";
+
+  //       $.each(payload, function(k, v) {
+  //         elemToAttach += '<div class="row"><div class="4u"><section class="highlight highlight-one"><a href="http://fav.me/d59i3b3" class="image image-full"><img src="images/pic02.jpg" alt=""></a><h3><a href="#">Aliquam diam consequat</a></h3><p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p></section></div><div class="4u"><section class="highlight highlight-two"><a href="http://fav.me/d4tqyby" class="image image-full"><img src="images/pic03.jpg" alt=""></a><h3><a href="#">Nisl adipiscing sed lorem</a></h3><p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p></section></div><div class="4u"><section class="highlight highlight-three"><a href="http://fav.me/d5w2dot" class="image image-full"><img src="images/pic04.jpg" alt=""></a><h3><a href="#">Mattis tempus lorem</a></h3><p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p></section></div></div>';
+  //         elemToAttach += "<section><h3 class='fa fa-check'>" + v.name + "</h3><p>" + v.description + " " + "$" + v.price+".</p></section>";
+  //       });
+  //       mydiv.innerHTML = elemToAttach;
+  //       $('#features .feature-list').append(mydiv);
+
+  //     var allMerchants = payload.merchants;
+  //     var random = Math.floor(getRandomArbitrary(0, allMerchants.length));
+  //     var singleMerchant = allMerchants[random];
+  //     debugger;
+  //     var randomMenu;
+  //     var itemPayload;
+  //   });
+  // });
 
     $.post( "/planner/details", $( "#deliveryForm" ).serialize(), function(data) {
        console.log("success!");
@@ -76,6 +87,10 @@ $(document).ready(function() {
 
   $('#foodProceed').click(function(e) {
     scrollToElement('#booze');
+  });
+
+  $('#bigYes').click(function(e) {
+    scrollToElement('#finalSubmit');
   });
 
   $('#boozeProceed').click(function(e) {
