6485e90b2e6c998fac7f5d569d575af6acc28e70
Johan Mena <johan.menac@gmail.com>
2014-04-06 07:09:15 -0400

    Add scroll to function

--- public/js/main.js
+++ public/js/main.js
@@ -11,5 +11,18 @@ $(document).ready(function() {
     $.post( "/planner/details", $( "#deliveryForm" ).serialize(), function(data) {
        console.log("success!");
      });
+
+    scrollToElement('#food');
   });
+
+  function scrollToElement(selector, time, verticalOffset) {
+    time = typeof(time) != 'undefined' ? time : 1000;
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $(selector);
+    offset = element.offset();
+    offsetTop = offset.top + verticalOffset;
+    $('html, body').animate({
+        scrollTop: offsetTop
+    }, time);
+  }
 });
